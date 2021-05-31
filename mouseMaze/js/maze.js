f709476dee0a282c3a209910d7523737ab7cad2b
Hira Kafle <16020447+hiradadiamond@users.noreply.github.com>
2021-05-30 19:04:43 -0500

    modified jquery functions

--- mouseMaze/js/maze.js
+++ mouseMaze/js/maze.js
@@ -1,27 +1,35 @@
 var win = true;
-
 $(document).ready(function () {
-  $("p").append("Yes!!");
-  $("#maze div.boundary").mouseover(lose);
-  $("#end").mouseover(end);
+  $("#maze div.boundary").off();
+  $("#end").off();
   $("#start").click(reset);
 
-  function lose(){
-    $("#boundary1").addClass("youlose");
-    $("#boundary2").addClass("youlose");
-    $("#boundary3").addClass("youlose");
-    $("#boundary4").addClass("youlose");
-    $("#boundary5").addClass("youlose");
-    alert(" Sorry, you lost :[");
+  var statusText = $("#status").text();
+
+  function red() {
+    win = false;
+    statusText = "Sorry, you lost";
+    $("#status").html(statusText).css("color", "red");
+    $(".boundary").each(function () {
+      $(this).addClass("youlose");
+    });
   }
+
   function reset() {
-    $("#boundary1").removeClass("youlose");
-    $("#boundary2").removeClass("youlose");
-    $("#boundary3").removeClass("youlose");
-    $("#boundary4").removeClass("youlose");
-    $("#boundary5").removeClass("youlose");
+    statusText = "Game Started!";
+    $("#status").html(statusText).css("color", "green");
+    $(".boundary").each(function () {
+      $(this).removeClass("youlose");
+    });
+    $("#maze div.boundary").on("mouseover", red);
+    $("#end").on("mouseover", end);
   }
   function end() {
-    alert("You Win!! :]");
+    statusText = "Game End!";
+    $("#status").html(statusText).css("color", "blue");
+    let currentStatus = win == true ? "You win! :]" : "Sorry, you lost! :[";
+    alert(currentStatus);
+    $(this).off();
+    $("#maze div.boundary").off();
   }
 });
