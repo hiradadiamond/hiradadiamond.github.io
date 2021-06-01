7c3229b319bff3bcecbfccf8f195a3a8d319c852
Hira Kafle <16020447+hiradadiamond@users.noreply.github.com>
2021-06-01 09:11:57 -0500

    updated shuffle algorithm

--- homework9/js/fifteen.js
+++ homework9/js/fifteen.js
@@ -31,12 +31,49 @@ $(document).ready(function () {
 
   // <!--Shuffle  Algorithim -->
   $("#shufflebutton").click(function () {
-    $("#puzzlearea div").each(function (idx) {
-      if (!topLeft.contains(idx)) {
-        console.log("idx" + this.x);
-      } else {
-        console.log("this" + this.x);
+    let originalUnshuffledArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    var shuffleArray = function (arr) {
+      let numElements = arr.length;
+      let numTimesToShuffle = numElements;
+      while (numTimesToShuffle != 0) {
+        let randIdx1 = Math.floor(Math.random() * numElements);
+        let randIdx2 = Math.floor(Math.random() * numElements);
+        let tmp = arr[randIdx1];
+        arr[randIdx1] = arr[randIdx2];
+        arr[randIdx2] = tmp;
+        numTimesToShuffle -= 1;
       }
+      return arr;
+    }
+    let shuffledArray = shuffleArray(originalUnshuffledArray)
+    console.log(shuffledArray);
+    $("#puzzlearea div").each(function (idx) {
+      console.log("Current idx is " + idx + " but we are going to plot " + shuffledArray[idx] + " index");
+      let newIdx = shuffledArray[idx];
+      var xPos = function (num) {
+        return (num % 4) * 100;
+      };
+      var yPos = function (num) {
+        return Math.floor(num / 4) * 100;
+      };
+      var x = xPos(newIdx);
+      var y = yPos(newIdx);
+      var origX = xPos(idx);
+      var origY = yPos(idx);
+      console.log(x);
+      console.log(y);
+      console.log("Orig pos");
+      console.log(origX);
+      console.log(origY);
+      $(this).addClass("puzzlepiece");
+      $(this).css({
+        left: x + "px",
+        top: y + "px",
+        "background-image": "url(images/background.jpg)",
+        "background-position": -origX + "px " + -origY + "px",
+      });
+      $(this).x = x;
+      $(this).y = y;
     });
   });
 
