af5402c0979d8549a8b551c882fa22e593cc1e29
Hira Kafle <16020447+hiradadiamond@users.noreply.github.com>
2021-06-05 10:08:41 -0500

    off shuffle button

--- homework9/js/fifteen.js
+++ homework9/js/fifteen.js
@@ -22,8 +22,8 @@ $(document).ready(function () {
       $(this).css({
         "background-image": "url(images/background.jpg)",
         "background-position": -x + "px " + -y + "px",
-        "left": x + "px",
-        "top": y + "px",
+        left: x + "px",
+        top: y + "px",
       });
       $(this).x = x;
       $(this).y = y;
@@ -31,36 +31,74 @@ $(document).ready(function () {
   };
   init();
 
-  //<!----UTILITY FUCNTIONS----->
-  var shuffleArray = function (arr) {
-    let numElements = arr.length;
-    let numTimesToShuffle = numElements;
-    while (numTimesToShuffle != 0) {
-      let randIdx1 = Math.floor(Math.random() * numElements);
-      let randIdx2 = Math.floor(Math.random() * numElements);
-      let tmp = arr[randIdx1];
-      arr[randIdx1] = arr[randIdx2];
-      arr[randIdx2] = tmp;
-      numTimesToShuffle -= 1;
+  //<!---- puzzle play -- ((hover and move )) --->
+  $("#puzzlearea div").hover(
+    function () {
+      $(this).addClass("movablepiece");
+    },
+    function () {
+      $(this).removeClass("movablepiece");
     }
-    return arr;
-  };
+  );
 
-  var randomizeEmptyArea = function (arr) {
-    let numElements = arr.length;
-    let randDivIdx = Math.floor(Math.random() * numElements);
-    var randDiv = $("#puzzlearea div")[randDivIdx];
-    console.log("Empty area is: x => " + row + " and y => " + col);
-    let randDivX = $(randDiv).position().left;
-    let randDivY = $(randDiv).position().top;
-    $(randDiv).css({
+  $("#puzzlearea div").click(function () {
+    var div = $(this);
+    var checkMov = isMovable(div);
+    if (checkMov) {
+      movePiece(div);
+    } else {
+      alert("Sorry, can't Move");
+    }
+  });
+
+  // <!--Shuffle  Algorithim -->
+  $("#shufflebutton").click(function () {
+    init();
+    let originalUnshuffledArray = [
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+    ];
+    let shuffledArray = shuffleArray(originalUnshuffledArray);
+    console.log(shuffledArray);
+    $("#puzzlearea div").each(function (idx) {
+      let newIdx = shuffledArray[idx];
+      var xPos = function (num) {
+        return (num % 4) * 100;
+      };
+      var yPos = function (num) {
+        return Math.floor(num / 4) * 100;
+      };
+      var x = xPos(newIdx);
+      var y = yPos(newIdx);
+      var origX = xPos(idx);
+      var origY = yPos(idx);
+      $(this).addClass("puzzlepiece");
+      $(this).css({
+        "background-image": "url(images/background.jpg)",
+        "background-position": -origX + "px " + -origY + "px",
+        left: x + "px",
+        top: y + "px",      
+      });
+      $(this).x = x;
+      $(this).y = y;
+    });
+    randomizeEmptyArea(shuffledArray);
+  });
+
+  //<!----UTILITY FUCNTIONS----->
+  function movePiece(div) {
+    var curElemPosition = div.position();
+    var x = curElemPosition.left;
+    var y = curElemPosition.top;
+    console.log(x + "x" + "y" + y);
+    var tempX = x / 100;
+    var tempY = y / 100;
+    $(div).css({
       top: col * HEIGHT,
       left: row * WIDTH,
     });
-    row = randDivX / 100;
-    col = randDivY / 100;
-    console.log("Empty area is: x => " + row + " and y => " + col);
-  };
+    row = tempX;
+    col = tempY;
+  }
 
   var isMovable = function (div) {
     var emptySquareXpos = row * WIDTH;
@@ -91,73 +129,33 @@ $(document).ready(function () {
     }
   };
 
-  // <!--Shuffle  Algorithim -->
-  $("#shufflebutton").click(function () {
-    init();
-    let originalUnshuffledArray = [
-      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-    ];
-    let shuffledArray = shuffleArray(originalUnshuffledArray);
-    console.log(shuffledArray);
-    $("#puzzlearea div").each(function (idx) {
-      let newIdx = shuffledArray[idx];
-      var xPos = function (num) {
-        return (num % 4) * 100;
-      };
-      var yPos = function (num) {
-        return Math.floor(num / 4) * 100;
-      };
-      var x = xPos(newIdx);
-      var y = yPos(newIdx);
-      var origX = xPos(idx);
-      var origY = yPos(idx);
-      $(this).addClass("puzzlepiece");
-      $(this).css({
-        left: x + "px",
-        top: y + "px",
-        "background-image": "url(images/background.jpg)",
-        "background-position": -origX + "px " + -origY + "px",
-      });
-      $(this).x = x;
-      $(this).y = y;
-    });
-    randomizeEmptyArea(shuffledArray);
-  });
-
-//<!---- puzzle play -- ((hover and move )) --->
-
-  $("#puzzlearea div").hover(
-    function () {
-      $(this).addClass("movablepiece");
-    },
-    function () {
-      $(this).removeClass("movablepiece");
+  var shuffleArray = function (arr) {
+    let numElements = arr.length;
+    let numTimesToShuffle = numElements;
+    while (numTimesToShuffle != 0) {
+      let randIdx1 = Math.floor(Math.random() * numElements);
+      let randIdx2 = Math.floor(Math.random() * numElements);
+      let tmp = arr[randIdx1];
+      arr[randIdx1] = arr[randIdx2];
+      arr[randIdx2] = tmp;
+      numTimesToShuffle -= 1;
     }
-  );
+    return arr;
+  };
 
-  $("#puzzlearea div").click(function () {
-    $("#shufflebutton").off();
-    var div = $(this);
-    var checkMov = isMovable(div);
-    if (checkMov) {
-      movePiece(div);
-    } else {
-      alert("Sorry, can't Move");
-    }
-  });
-
-  function movePiece(div) {
-    var curElemPosition = div.position();
-    var x = curElemPosition.left;
-    var y = curElemPosition.top;
-    console.log(x + "x" + "y" + y);
-    var tempX = x / 100;
-    var tempY = y / 100;
-    $(div).css({
+  var randomizeEmptyArea = function (arr) {
+    let numElements = arr.length;
+    let randDivIdx = Math.floor(Math.random() * numElements);
+    var randDiv = $("#puzzlearea div")[randDivIdx];
+    console.log("Empty area is: x => " + row + " and y => " + col);
+    let randDivX = $(randDiv).position().left;
+    let randDivY = $(randDiv).position().top;
+    $(randDiv).css({
       top: col * HEIGHT,
       left: row * WIDTH,
     });
-    row = tempX;
-    col = tempY;
-  }
+    row = randDivX / 100;
+    col = randDivY / 100;
+    console.log("Empty area is: x => " + row + " and y => " + col);
+  };
 });
